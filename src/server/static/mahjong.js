ff130e3bb35f14a3cd8f1cd813325f6545f73d0e
faraplay <faraplay@outlook.com>
2025-08-28 14:56:34 +0100

    move javascript to separate file

---
+++ src/server/static/mahjong.js
@@ -0,0 +1,44 @@
+
+const socket = io();
+
+const action_types = [
+    "NOTHING",
+    "DRAW",
+    "DISCARD",
+    "CHI_A",
+    "CHI_B",
+    "CHI_C",
+    "PON",
+    "OPEN_KAN",
+    "ADD_KAN",
+    "CLOSED_KAN",
+    "FLOWER",
+    "RON",
+    "TSUMO"
+];
+
+const set_player_form = document.getElementById('set_player_form');
+const set_player_select = document.getElementById('set_player_select');
+const history_list = document.getElementById('history_list');
+
+set_player_form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (set_player_select.value) {
+        socket.emit('set_player', set_player_select.value);
+    }
+});
+
+socket.on('list_all', (history) => {
+    console.log(history);
+    for (const playeraction of history) {
+        console.log(playeraction)
+        const player = playeraction[0];
+        const action = playeraction[1];
+        const action_type = action_types[action[0]];
+        const tile = action[1];
+        const item = document.createElement('li');
+        item.textContent = `Player: ${player}, Action: ${action_type}` + 
+        (tile != 0 ? `, Tile: ${tile}` : "");
+        history_list.appendChild(item);
+    }
+});
