9a44349696fc3d0c514c1ccf7b416d874d5165e8
faraplay <faraplay@outlook.com>
2025-09-02 11:03:53 +0100

    write some game tests

--- src/mahjong/game.py
+++ src/mahjong/game.py
@@ -1,10 +1,15 @@
+from .tile import Tile
 from .game_options import GameOptions
 from .round import Round
 from .scoring import ScoringHand
 
 
 class Game:
-    def __init__(self, options: GameOptions):
+    def __init__(
+        self,
+        first_deck_tiles: list[Tile] | None = None,
+        options: GameOptions = GameOptions(),
+    ):
         self._player_count = options.player_count
         self._options = options
         self._wind_round = 0
@@ -12,7 +17,19 @@ class Game:
         self._player_seats = tuple(range(self._player_count))
         self._player_scores = [options.start_score] * self._player_count
         self._draw_count = 0
-        self._create_round()
+        self._create_round(first_deck_tiles)
+
+    @property
+    def wind_round(self):
+        return self._wind_round
+
+    @property
+    def sub_round(self):
+        return self._sub_round
+
+    @property
+    def round(self):
+        return self._round
 
     @property
     def player_scores(self):
@@ -28,7 +45,7 @@ class Game:
     def get_player(self, seat: int):
         return (seat - self._sub_round) % self._player_count
 
-    def start_next_round(self):
+    def start_next_round(self, deck_tiles: list[int] | None = None):
         if self.is_dealer_repeat():
             self._sub_round += 1
             if self._sub_round >= self._player_count:
@@ -42,7 +59,7 @@ class Game:
             self._draw_count += 1
         else:
             self._draw_count = 0
-        self._create_round()
+        self._create_round(deck_tiles)
 
     def is_dealer_repeat(self):
         if self._win_scoring is None:
@@ -50,12 +67,14 @@ class Game:
         else:
             return self._win_scoring.win.win_seat == 0
 
-    def _create_round(self):
+    def _create_round(self, deck_tiles: list[Tile] | None):
         def self_calculate_win_score():
             self._calculate_win_score()
 
         self._round = Round(
-            options=self._options, round_end_callback=self_calculate_win_score
+            tiles=deck_tiles,
+            options=self._options,
+            round_end_callback=self_calculate_win_score,
         )
         self._win_scoring = None
 
---
+++ tests/game_test.py
@@ -0,0 +1,25 @@
+import unittest
+
+from src.mahjong.action import Action, ActionType
+from src.mahjong.round import RoundStatus
+from src.mahjong.game import Game
+
+from decks import *
+
+
+class GameTest(unittest.TestCase):
+    def test_first_game(self):
+        game = Game(test_deck2)
+        self.assertEqual(game.wind_round, 0)
+        self.assertEqual(game.sub_round, 0)
+
+    def test_no_winscoring_during_first_game(self):
+        game = Game(test_deck2)
+        self.assertIsNone(game.win_scoring)
+
+    def test_auto_calculate_score(self):
+        game = Game(test_deck2)
+        game.round.do_action(0, Action(action_type=ActionType.DISCARD, tile=13))
+        game.round.do_action(2, Action(action_type=ActionType.RON))
+        self.assertEqual(game.round.status, RoundStatus.END)
+        self.assertIsNotNone(game.win_scoring)
