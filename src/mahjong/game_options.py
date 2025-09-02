7752bb595d02b27b6898ae99ad6d70723fdf8e8a
faraplay <faraplay@outlook.com>
2025-09-02 17:14:40 +0100

    use decorator for yaku, remove yaku enum

--- src/mahjong/game_options.py
+++ src/mahjong/game_options.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
-from .win_info import Yaku, default_yaku_values
+from .win_info import default_yaku_han
 
 
 class GameOptions(BaseModel):
@@ -9,7 +9,7 @@ class GameOptions(BaseModel):
     end_wall_count: int = 14
     game_length: tuple[int, int] = (1, 0)
     start_score: int = 0
-    yaku_values: dict[Yaku, int] = default_yaku_values
+    yaku_values: dict[str, int] = default_yaku_han
     score_dealer_win_base_value: int = 3
     score_nondealer_win_base_value: int = 2
     score_dealer_pay_in_base_value: int = 4
--- src/mahjong/win_info.py
+++ src/mahjong/win_info.py
@@ -1,4 +1,3 @@
-from enum import StrEnum
 from pydantic import BaseModel
 
 from .tile import Tile, is_number
@@ -12,18 +11,25 @@ class Win(BaseModel):
     calls: list[Call]
 
 
-class Yaku(StrEnum):
-    EYES = "eyes"
-    SEVEN_PAIRS = "seven pairs"
-    THIRTEEN_ORPHANS = "thirteen orphans"
-
-
 class YakuHan(BaseModel):
-    yaku: Yaku
+    yaku: str
     han: int
 
 
-default_yaku_values = {Yaku.EYES: 1, Yaku.SEVEN_PAIRS: 3, Yaku.THIRTEEN_ORPHANS: 13}
+yaku_display_names: dict[str, str] = {}
+default_yaku_han: dict[str, int] = {}
+is_yaku_functions = {}
+
+
+def _register_yaku(name: str, display_name: str, han: int):
+    yaku_display_names[name] = display_name
+    default_yaku_han[name] = han
+
+    def _register_yaku_inner(func):
+        is_yaku_functions[name] = func
+        return func
+
+    return _register_yaku_inner
 
 
 class YakuCalculator:
@@ -32,23 +38,26 @@ class YakuCalculator:
         self._formed_hand = formed_hand
 
     def get_yakus(self):
-        yakus: list[Yaku] = []
-        for yaku, is_yaku in self.is_yaku_functions.items():
+        yakus = []
+        for yaku, is_yaku in is_yaku_functions.items():
             if is_yaku(self):
                 yakus.append(yaku)
         return yakus
 
+    @_register_yaku("SEVEN_PAIRS", "Seven Pairs", 3)
     def _is_seven_pairs(self):
         return len(self._formed_hand) == 7 and all(
             call.call_type == CallType.PAIR for call in self._formed_hand
         )
 
+    @_register_yaku("THIRTEEN_ORPHANS", "Thirteen Orphans", 13)
     def _is_thirteen_orphans(self):
         return (
             len(self._formed_hand) == 1
             and self._formed_hand[0].call_type == CallType.THIRTEEN_ORPHANS
         )
 
+    @_register_yaku("EYES", "Eyes", 1)
     def _is_eyes(self):
         pairs = [call for call in self._formed_hand if call.call_type == CallType.PAIR]
         if len(pairs) != 1:
@@ -57,9 +66,3 @@ class YakuCalculator:
         if not is_number(tile):
             return False
         return (tile % 10) % 3 == 2
-
-    is_yaku_functions = {
-        Yaku.EYES: _is_eyes,
-        Yaku.SEVEN_PAIRS: _is_seven_pairs,
-        Yaku.THIRTEEN_ORPHANS: _is_thirteen_orphans,
-    }
