72a08f42baa8cbf7f8748fe0124b3cd50d35ff98
faraplay <faraplay@outlook.com>
2025-09-11 23:36:01 +0100

    use socketio builtin logger

--- src/server/main.py
+++ src/server/main.py
@@ -12,18 +12,16 @@ from .game_room import GameRoom
 
 @socketio.on("connect")
 def connect(auth):
-    print(f"Client connected: {request.sid},\nAuth: {auth}")
+    pass
 
 
 @socketio.on("disconnect")
 def disconnect(reason):
-    print(f"Client disconnected: {request.sid},\nReason: {reason}")
     remove_sid(request.sid)
 
 
 @socketio.on("action")
 def handle_action(player_data, action_data):
-    print(f"Received action from {request.sid}: {player_data}, {action_data}")
     player = Player.model_validate(player_data)
     game_room = GameRoom.get_player_room(player)
     if game_room is None:
@@ -51,7 +49,6 @@ def start_next_round(player_data):
 @socketio.on("set_name")
 def on_set_name(name):
     verify_name(name)
-    print(f"Received set_name from {request.sid}: {name}")
     player = Player.from_name(name)
     set_player(request.sid, player)
     game_room = GameRoom.get_player_room(player)
@@ -72,7 +69,6 @@ def on_get_rooms():
 def on_create_room(room_name, player_count):
     GameRoom.verify_player_count(player_count)
     GameRoom.verify_room_name(room_name)
-    print(f"Received create_room from {request.sid}: {room_name} {player_count}")
     player = get_player(request.sid)
     return GameRoom.create_room(player, room_name, player_count).room_info
 
@@ -80,21 +76,18 @@ def on_create_room(room_name, player_count):
 @socketio.on("join_room")
 def on_join_room(room_name):
     GameRoom.verify_room_name(room_name)
-    print(f"Received join_room from {request.sid}: {room_name}")
     player = get_player(request.sid)
     return GameRoom.join_room(player, room_name).room_info
 
 
 @socketio.on("leave_room")
 def on_leave_room():
-    print(f"Received leave_room from {request.sid}")
     player = get_player(request.sid)
     return GameRoom.leave_room(player).room_info
 
 
 @socketio.on("start_game")
 def on_start_game(room_name, form_data):
-    print(f"Received start_game from {request.sid}")
     player = get_player(request.sid)
     game_room = GameRoom.get_player_room(player)
     if game_room is None or room_name != game_room.room_name:
--- src/server/socketio.py
+++ src/server/socketio.py
@@ -2,4 +2,4 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 app = Flask(__name__, static_folder="../../static")
-socketio = SocketIO(app)
+socketio = SocketIO(app, logger=True)
