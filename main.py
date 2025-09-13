27981ab29babf45c98be4f2138019376654d767c
faraplay <faraplay@outlook.com>
2025-09-13 12:18:07 +0100

    use werkzeug to run debug server

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from src.server.main import run_server
+from werkzeug import run_simple
+from src.server import debug_app
 
 if __name__ == "__main__":
-    run_server()
+    run_simple("localhost", 5000, debug_app, use_reloader=True, threaded=True)
