dc5d1bbf8edb2172b0a50685e3f132eb589d5260
faraplay <faraplay@outlook.com>
2025-09-17 15:47:45 +0100

    add loggers for server and root

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -1,3 +1,7 @@
-from . import logger
+from .logger import create_server_logger, create_root_logger
+
+create_root_logger()
+create_server_logger(__name__)
+
 from .sio import debug_app, app
 from . import main
--- src/server/logger.py
+++ src/server/logger.py
@@ -1,21 +1,36 @@
 import logging
 
-# logging.basicConfig(filename="server.log", encoding="utf-8", level=logging.INFO)
 
-logger = logging.getLogger()
+def create_server_logger(name: str):
+    logger = logging.getLogger(name)
 
-formatter = logging.Formatter(
-    fmt="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
-    datefmt="%d/%b/%Y %H:%M:%S",
-)
+    formatter = logging.Formatter(
+        fmt="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+        datefmt="%d/%b/%Y %H:%M:%S",
+    )
 
-debug_handler = logging.FileHandler("debug.log", encoding="utf-8")
-debug_handler.setLevel(logging.DEBUG)
-debug_handler.setFormatter(formatter)
+    debug_handler = logging.FileHandler("server_debug.log", encoding="utf-8")
+    debug_handler.setLevel(logging.DEBUG)
+    debug_handler.setFormatter(formatter)
 
-info_handler = logging.FileHandler("server.log", encoding="utf-8")
-info_handler.setLevel(logging.INFO)
-info_handler.setFormatter(formatter)
+    info_handler = logging.FileHandler("server_info.log", encoding="utf-8")
+    info_handler.setLevel(logging.INFO)
+    info_handler.setFormatter(formatter)
 
-logger.addHandler(debug_handler)
-logger.addHandler(info_handler)
+    logger.addHandler(debug_handler)
+    logger.addHandler(info_handler)
+
+
+def create_root_logger():
+    logger = logging.getLogger()
+
+    formatter = logging.Formatter(
+        fmt="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+        datefmt="%d/%b/%Y %H:%M:%S",
+    )
+
+    info_handler = logging.FileHandler("info.log", encoding="utf-8")
+    info_handler.setLevel(logging.INFO)
+    info_handler.setFormatter(formatter)
+
+    logger.addHandler(info_handler)
