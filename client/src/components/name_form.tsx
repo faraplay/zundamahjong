d4f5810b82a38209448974693585059e29e59317
faraplay <faraplay@outlook.com>
2025-09-20 22:44:50 +0100

    create NameForm component

---
+++ client/src/components/name_form.tsx
@@ -0,0 +1,12 @@
+import './name_form.css'
+
+export function NameForm() {
+    return <form id="name_form" action="">
+        Enter a name
+        <div>
+            <label for="name_input">Name</label>
+            <input id="name_input" type="text" />
+        </div>
+        <button type="submit">Set name</button>
+    </form>
+}
