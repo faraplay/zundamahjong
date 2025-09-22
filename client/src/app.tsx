06839808c297adc91655dfe4304560d17acf1985
faraplay <faraplay@outlook.com>
2025-09-22 13:29:48 +0100

    connect to socket io

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,11 +1,22 @@
+import { useRef, useEffect, type MutableRef } from "preact/hooks";
+import { io, Socket } from "socket.io-client";
 import { NameForm } from "./components/name_form";
 import "./app.css";
 
 export function App() {
+  const socket = useRef() as MutableRef<Socket>;
+  useEffect(() => {
+    socket.current = io();
+    return () => {
+      socket.current.disconnect();
+    };
+  }, []);
   return (
     <>
       <div id="name_screen" class="screen">
-        <NameForm />
+        <NameForm
+          emit={(event, ...args) => socket.current.emit(event, ...args)}
+        />
       </div>
     </>
   );
--- client/src/components/name_form.tsx
+++ client/src/components/name_form.tsx
@@ -1,12 +1,24 @@
 import "./name_form.css";
 
-export function NameForm() {
+export function NameForm({
+  emit,
+}: {
+  emit: (event: string, ...args: any[]) => any;
+}) {
+  const onSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+    const name = formData.get("name");
+    if (name) {
+      emit("set_name", name);
+    }
+  };
   return (
-    <form id="name_form" action="">
+    <form id="name_form" action="" onSubmit={onSubmit}>
       Enter a name
       <div>
         <label for="name_input">Name</label>
-        <input id="name_input" type="text" />
+        <input id="name_input" name="name" type="text" />
       </div>
       <button type="submit">Set name</button>
     </form>
