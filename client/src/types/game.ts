c5fe7e8a060f2de2e8637ebc59911acf2b92824d
faraplay <faraplay@outlook.com>
2025-09-23 16:11:14 +0100

    add types used in game

---
+++ client/src/types/game.ts
@@ -0,0 +1,112 @@
+export type Tile = number;
+
+export type Discard = {
+  player: number;
+  tile: Tile;
+};
+
+export const enum CallType {
+  PAIR = 0,
+  CHI = 1,
+  PON = 2,
+  OPEN_KAN = 3,
+  ADD_KAN = 4,
+  CLOSED_KAN = 5,
+  THIRTEEN_ORPHANS = 6,
+}
+
+export type Call = {
+  call_type: CallType;
+  tiles: Tile[];
+};
+
+export const enum ActionType {
+  PASS = 0,
+  CONTINUE = 1,
+  DRAW = 2,
+  DISCARD = 3,
+  CHI_A = 4,
+  CHI_B = 5,
+  CHI_C = 6,
+  PON = 7,
+  OPEN_KAN = 8,
+  ADD_KAN = 9,
+  CLOSED_KAN = 10,
+  FLOWER = 11,
+  RON = 12,
+  TSUMO = 13,
+}
+
+export type Action = {
+  action_type: ActionType;
+  tile: Tile;
+};
+
+export type HistoryItem = {
+  player: number;
+  action: Action;
+};
+
+export const enum RoundStatus {
+  START = 0,
+  PLAY = 1,
+  CALLED_PLAY = 2,
+  ADD_KAN_AFTER = 3,
+  CLOSED_KAN_AFTER = 4,
+  DISCARDED = 5,
+  LAST_DISCARDED = 6,
+  END = 7,
+}
+
+export type GameInfo = {
+  player_names: string[];
+  wind_round: number;
+  sub_round: number;
+  draw_count: number;
+  player_scores: number[];
+};
+
+export type RoundInfo = {
+  tiles_left: number;
+  current_player: number;
+  status: RoundStatus;
+  hand_counts: number[];
+  discards: Discard[];
+  calls: Call[][];
+  flowers: Tile[][];
+  history: HistoryItem[];
+};
+
+export type PlayerInfo = {
+  hand: Tile[];
+  last_tile: Tile;
+  actions: Action[];
+};
+
+export type Win = {
+  win_player: number;
+  lose_player: number | undefined;
+  hand: Tile[];
+  calls: Call[];
+  flowers: Tile[];
+};
+
+export type Scoring = {
+  win_player: number;
+  lose_player: number | undefined;
+  yaku_hans: { [yaku: string]: number };
+  han_total: number;
+  player_scores: number[];
+};
+
+export type AllInfo = {
+  player_count: number;
+  player_index: number;
+  is_game_end: boolean;
+  game_info: GameInfo;
+  round_info: RoundInfo;
+  history_updates: HistoryItem[];
+  player_info: PlayerInfo;
+  win_info: Win;
+  scoring_info: Scoring;
+};
