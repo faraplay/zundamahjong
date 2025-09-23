4a05b5f6165f3d0878e31645b4bddd83759bafd4
faraplay <faraplay@outlook.com>
2025-09-23 22:30:15 +0100

    add status class to game screen

--- client/src/components/game/game_screen/game_screen.tsx
+++ client/src/components/game/game_screen/game_screen.tsx
@@ -4,7 +4,9 @@ import { Table } from "../table/table";
 
 export function GameScreen({ info }: { info: AllInfo }) {
   return (
-    <div class={`me_player_${info.player_index}`}>
+    <div
+      class={`me_player_${info.player_index} status_${info.round_info.status}`}
+    >
       <Table info={info} />
     </div>
   );
