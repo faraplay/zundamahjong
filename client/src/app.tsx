ebd31fb3359dbd2bc55f9cb5b482674e03cc2561
faraplay <faraplay@outlook.com>
2025-09-26 19:57:47 +0100

    add error messages component

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,12 +1,15 @@
 import { useRef, useEffect, type MutableRef, useState } from "preact/hooks";
 import { io, Socket } from "socket.io-client";
 
+import type { ErrorMessage } from "./types/error_message";
 import type { Player } from "./types/player";
 import type { Room } from "./types/room";
 import { RoundStatus, type AllInfo } from "./types/game";
 import type { EmitFunc } from "./types/emit_func";
 
 import { Emitter } from "./components/emitter/emitter";
+
+import { ErrorList } from "./components/error_list/error_list";
 import { NameForm } from "./components/name_form/name_form";
 import { JoinRoomForm } from "./components/join_room_form/join_room_form";
 import { CreateRoomForm } from "./components/create_room_form/create_room_form";
@@ -19,20 +22,34 @@ import "./fonts.css";
 import "./app.css";
 
 export function App() {
-  const socket = useRef() as MutableRef<Socket>;
-  const emit: EmitFunc = (event, ...args) =>
-    socket.current.emit(event, ...args);
+  const [errors, setErrors] = useState<{
+    currentIndex: number;
+    list: ErrorMessage[];
+  }>({ currentIndex: 0, list: [] });
 
   const [myPlayer, setMyPlayer] = useState<Player>();
   const [rooms, setRooms] = useState<Array<Room>>([]);
   const [myRoom, setMyRoom] = useState<Room>();
+
   const [info, setInfo] = useState<AllInfo>();
   const [actionSubmitted, setActionSubmitted] = useState<boolean>(false);
   const [seeResults, setSeeResults] = useState(false);
 
+  const socket = useRef() as MutableRef<Socket>;
+  const emit: EmitFunc = (event, ...args) =>
+    socket.current.emit(event, ...args);
+
   useEffect(() => {
     socket.current = io();
 
+    socket.current.on("message", (message: string) => {
+      console.log(message);
+      setErrors((errors) => {
+        errors.list.push({ index: errors.currentIndex, message });
+        errors.currentIndex++;
+        return errors;
+      });
+    });
     socket.current.on("player_info", (player: Player) => {
       setMyPlayer(player);
     });
@@ -55,51 +72,79 @@ export function App() {
     };
   }, []);
 
+  const screen = getScreen(
+    myPlayer,
+    myRoom,
+    rooms,
+    info,
+    actionSubmitted,
+    setActionSubmitted,
+    seeResults,
+    setSeeResults,
+  );
+  return (
+    <Emitter.Provider value={emit}>
+      {screen}
+      <ErrorList
+        errors={errors.list}
+        removeError={(index) => {
+          setErrors({
+            currentIndex: errors.currentIndex,
+            list: errors.list.filter((error) => error.index != index),
+          });
+        }}
+      />
+    </Emitter.Provider>
+  );
+}
+
+function getScreen(
+  myPlayer: Player | undefined,
+  myRoom: Room | undefined,
+  rooms: Room[],
+  info: AllInfo | undefined,
+  actionSubmitted: boolean,
+  setActionSubmitted: (value: boolean) => void,
+  seeResults: boolean,
+  setSeeResults: (value: boolean) => void,
+) {
   if (!myPlayer) {
     return (
-      <Emitter.Provider value={emit}>
-        <div id="name_screen" class="screen">
-          <NameForm />
-        </div>
-      </Emitter.Provider>
+      <div id="name_screen" class="screen">
+        <NameForm />
+      </div>
     );
   }
   if (!myRoom) {
     return (
-      <Emitter.Provider value={emit}>
-        <div id="lobby_screen" class="screen">
-          <JoinRoomForm rooms={rooms} />
-          <CreateRoomForm />
-        </div>
-      </Emitter.Provider>
+      <div id="lobby_screen" class="screen">
+        <JoinRoomForm rooms={rooms} />
+        <CreateRoomForm />
+      </div>
     );
   }
   if (!info) {
     return (
-      <Emitter.Provider value={emit}>
-        <div id="room_screen" class="screen">
-          <RoomInfo room={myRoom} />
-          {myRoom && myRoom.joined_players[0].id == myPlayer.id ? (
-            <GameOptionsForm
-              player_count={myRoom.player_count}
-              can_start={myRoom.joined_players.length == myRoom.player_count}
-            />
-          ) : (
-            <></>
-          )}
-        </div>
-      </Emitter.Provider>
+      <div id="room_screen" class="screen">
+        <RoomInfo room={myRoom} />
+        {myRoom && myRoom.joined_players[0].id == myPlayer.id ? (
+          <GameOptionsForm
+            player_count={myRoom.player_count}
+            can_start={myRoom.joined_players.length == myRoom.player_count}
+          />
+        ) : (
+          <></>
+        )}
+      </div>
     );
   }
   return (
-    <Emitter.Provider value={emit}>
-      <GameScreen
-        info={info}
-        actionSubmitted={actionSubmitted}
-        setActionSubmitted={() => setActionSubmitted(true)}
-        seeResults={seeResults}
-        goToResults={() => setSeeResults(true)}
-      />
-    </Emitter.Provider>
+    <GameScreen
+      info={info}
+      actionSubmitted={actionSubmitted}
+      setActionSubmitted={() => setActionSubmitted(true)}
+      seeResults={seeResults}
+      goToResults={() => setSeeResults(true)}
+    />
   );
 }
