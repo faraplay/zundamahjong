7fa08338f9f0b52360a0a4464d7ff1f1c9e97b75
faraplay <faraplay@outlook.com>
2025-09-27 20:48:20 +0100

    add fourth avatar

--- client/src/types/avatars.ts
+++ client/src/types/avatars.ts
@@ -4,6 +4,8 @@ import metanURL from "../assets/avatars/metan.png";
 import metanFaceURL from "../assets/avatars/metan_face.png";
 import tsumugiURL from "../assets/avatars/tsumugi.png";
 import tsumugiFaceURL from "../assets/avatars/tsumugi_face.png";
+import kiritanURL from "../assets/avatars/kiritan.png";
+import kiritanFaceURL from "../assets/avatars/kiritan_face.png";
 
 export type AvatarIdDict = { [playerId: string]: number };
 
@@ -11,4 +13,5 @@ export const avatars = [
   { name: "Zundamon", imageURL: zundamonURL, faceURL: zundamonFaceURL },
   { name: "Metan", imageURL: metanURL, faceURL: metanFaceURL },
   { name: "Tsumugi", imageURL: tsumugiURL, faceURL: tsumugiFaceURL },
+  { name: "Kiritan", imageURL: kiritanURL, faceURL: kiritanFaceURL },
 ] as const;
