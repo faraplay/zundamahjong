21bac36e48384759bfb75597ed93d902908decc0
Alex Epelde <alex@epelde.net>
2025-09-29 18:10:36 -0400

    allow custom debug_server port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,24 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import preact from "@preact/preset-vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/zundamahjong/",
-  plugins: [preact()],
-  build: {
-    outDir: "../client_build",
-    emptyOutDir: true,
-    assetsInlineLimit: (filePath) =>
-      filePath.endsWith(".svg") &&
-      !filePath.includes("/flower/") &&
-      !filePath.includes("/season/"),
-  },
-  server: {
-    proxy: {
-      "/socket.io": {
-        target: "http://localhost:5000",
-        ws: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const debug_server_port = env.DEBUG_SERVER_PORT
+    ? Number(env.DEBUG_SERVER_PORT) : 5000;
+  return {
+    base: "/zundamahjong/",
+    plugins: [preact()],
+    build: {
+      outDir: "../client_build",
+      emptyOutDir: true,
+      assetsInlineLimit: (filePath) =>
+        filePath.endsWith(".svg") &&
+        !filePath.includes("/flower/") &&
+        !filePath.includes("/season/"),
+    },
+    server: {
+      proxy: {
+        "/socket.io": {
+          target: `http://localhost:${debug_server_port}`,
+          ws: true,
+        },
       },
     },
-  },
+  };
 });
