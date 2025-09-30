40d018050d567201d4df9bc1395f580147e2567e
faraplay <faraplay@outlook.com>
2025-09-30 16:16:10 +0100

    write test to test all possible suit hands

--- tests/shanten_test.py
+++ tests/shanten_test.py
@@ -95,15 +95,50 @@ class ShantenTest(unittest.TestCase):
         )
 
     def test_shanten_times_1000(self):
-        datas = []
         for _ in range(1000):
-            datas.append(suit_shanten_data([2, 3, 3, 4, 4, 4, 5, 5, 6]))
+            suit_shanten_data([2, 3, 3, 4, 4, 4, 5, 5, 6])
 
     def test_shanten_long_times_1000(self):
-        datas = []
         for _ in range(1000):
-            datas.append(
-                suit_shanten_data(
-                    [2, 3, 3, 4, 4, 4, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 9, 9, 9]
-                )
-            )
+            suit_shanten_data([2, 3, 3, 4, 4, 4, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 9, 9, 9])
+
+    def test_shanten_all(self):
+        def hand_from_code(hand_code):
+            hand = []
+            for tile in range(1, 10):
+                for _ in range(hand_code % 5):
+                    hand.append(tile)
+                hand_code //= 5
+            return hand
+
+        def code_from_hand(hand):
+            return sum(5 ** (i - 1) for i in hand)
+
+        limit = 1000
+        datas = []
+        for hand_code in range(limit):
+            hand = hand_from_code(hand_code)
+            assert hand_code == code_from_hand(hand)
+            datas.append(suit_shanten_data(hand))
+
+        for hand_code, data in enumerate(datas):
+            for tile in range(1, 10):
+                if hand_from_code(hand_code).count(tile) == 4:
+                    continue
+                mask = 0b1_000_000_000 >> tile
+                added_hand_code = hand_code + (5 ** (tile - 1))
+                if added_hand_code >= limit:
+                    break
+                for k in range(10):
+                    if data[k][1] & mask:
+                        self.assertEqual(
+                            data[k][0] + 1,
+                            datas[added_hand_code][k][0],
+                            f"hand is {hand_from_code(hand_code)}, tile is {tile}, k is {k}",
+                        )
+                    else:
+                        self.assertEqual(
+                            data[k][0],
+                            datas[added_hand_code][k][0],
+                            f"hand is {hand_from_code(hand_code)}, tile is {tile}, k is {k}",
+                        )
