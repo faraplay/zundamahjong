0b76b6113ca4e70ebf0a34b4511d574c7e1a3c4d
faraplay <faraplay@outlook.com>
2025-10-01 16:54:37 +0100

    use frozenset in tile.py

--- src/mahjong/tile.py
+++ src/mahjong/tile.py
@@ -43,8 +43,8 @@ all_tiles = frozenset(
     + [21, 22, 23, 24, 25, 26, 27, 28, 29]
     + [31, 32, 33, 34, 35, 36, 37]
 )
-orphans = {1, 9, 11, 19, 21, 29, 31, 32, 33, 34, 35, 36, 37}
-terminals = {1, 9, 11, 19, 21, 29}
-winds = {31, 32, 33, 34}
-dragons = {35, 36, 37}
-green_tiles = {22, 23, 24, 26, 28, 36}
+orphans = frozenset({1, 9, 11, 19, 21, 29, 31, 32, 33, 34, 35, 36, 37})
+terminals = frozenset({1, 9, 11, 19, 21, 29})
+winds = frozenset({31, 32, 33, 34})
+dragons = frozenset({35, 36, 37})
+green_tiles = frozenset({22, 23, 24, 26, 28, 36})
