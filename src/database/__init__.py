c1788ad7682ee7c6f460b9939aedfd754426ac78
Alex Epelde <alex@epelde.net>
2025-10-01 12:57:26 -0400

    basic password hashing

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,5 +1,7 @@
 import logging
+import hashlib
 import os
+import secrets
 
 from sqlalchemy import String, create_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column, sessionmaker
@@ -38,3 +40,33 @@ def get_db(sid: str) -> Session:
         if "db" not in session:
             session["db"] = db_factory()
         return session["db"]
+
+
+def _hash_internal(password: str, salt: str, method: str) -> str:
+    method, *args = method.split(":")
+
+    if method != "scrypt":
+        raise NotImplementedError
+
+    n, r, p = map(int, args)
+    maxmem = 132 * n * r * p
+
+    return hashlib.scrypt(
+        password.encode(), salt=salt.encode(), n=n, r=r, p=p, maxmem=maxmem
+    ).hex()
+
+
+def hash_pw(password: str, salt: str | None, method: str | None) -> str:
+    if not salt:
+        salt = secrets.token_urlsafe(16)
+
+    if not method:
+        method = "scrypt:32768:8:1"
+
+    hashval = _hash_internal(password, salt, method)
+    return f"{method}${salt}${hashval}"
+
+
+def check_pw(password: str, pwhash: str) -> bool:
+    method, salt, hashval = pwhash.split("$", 2)
+    return hashval == _hash_internal(password, salt, method)
