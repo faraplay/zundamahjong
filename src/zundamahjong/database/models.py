0c2af38b25bb063aa0d8a8d5c85802845a64e96f
Alex Epelde <alex@epelde.net>
2025-10-12 19:06:48 -0400

    add avatar column to database
    change avatar into Enum (db side)
    
    squash alembic revisions

--- src/zundamahjong/database/models.py
+++ src/zundamahjong/database/models.py
@@ -1,14 +1,23 @@
-from sqlalchemy import String
+from typing import final
+
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from ..types.avatar import Avatar
 
 
 class Base(DeclarativeBase):
     pass
 
 
+@final
 class User(Base):
     __tablename__ = "user_account"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String, unique=True)
-    password: Mapped[str] = mapped_column(String)
+    name: Mapped[str] = mapped_column(unique=True)
+
+    """Hashed user password."""
+    password: Mapped[str] = mapped_column()
+
+    """User's chosen avatar."""
+    avatar: Mapped[Avatar] = mapped_column(server_default=("zundamon"))
