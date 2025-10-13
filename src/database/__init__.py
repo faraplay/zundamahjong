c6d929153069d8fecf6011a3534339355a8a68c0
Alex Epelde <alex@epelde.net>
2025-10-13 02:24:54 -0400

    satisfy Python 3.9

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import sqlalchemy as sa
 from sqlalchemy.orm import Session
 
@@ -19,5 +21,5 @@ def close_db(sid: str) -> None:
             session["db"].close()
 
 
-def get_user(db: Session, name: str) -> User | None:
+def get_user(db: Session, name: str) -> Optional[User]:
     return db.execute(sa.select(User).where(User.name == name)).scalar_one_or_none()
