8bb832dabb789fb389eaff8edb0c2cc882a4e367
Alex Epelde <alex@epelde.net>
2025-10-14 02:33:45 -0400

    include client files in wheel

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os.path
+import shutil
+
+from setuptools import Command, setup
+from setuptools.command.build import build
+
+
+class build_client(Command):
+    def initialize_options(self) -> None:
+        self.client_dir = None
+        self.editable_mode = False
+
+    def finalize_options(self) -> None:
+        bdist_dir = self.get_finalized_command("bdist_wheel").bdist_dir
+
+        if bdist_dir:
+            self.client_dir = os.path.join(bdist_dir, "zundamahjong", "client")
+
+    def run(self) -> None:
+        """Build the Zundamahjong static client files."""
+
+        if not self.editable_mode:
+            os.chdir("client")
+
+            self.spawn(["npm", "install"])
+            self.spawn(["npm", "run", "build"])
+
+            os.chdir("..")
+
+            if self.client_dir:
+                shutil.move("client_build", self.client_dir)
+
+
+class custom_build(build):
+    sub_commands = [("build_client", None)] + build.sub_commands
+
+
+setup(
+    cmdclass={
+        "build_client": build_client,
+        "build": custom_build,
+    },
+)
