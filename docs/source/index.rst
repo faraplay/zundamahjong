b278a8753ac36e53beed8e3986680fcb316412e2
Alex Epelde <alex@epelde.net>
2025-10-18 13:20:13 -0400

    docs: database

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,3 +3,15 @@ Internal API Reference
 
 .. toctree::
    :maxdepth: 3
+
+
+zundamahjong.database
+---------------------
+
+.. module:: zundamahjong.database
+
+.. autoclass:: SQLAlchemy
+   :members:
+
+.. _sqlalchemy.Engine: https://docs.sqlalchemy.org/en/latest/core/connections.html#sqlalchemy.engine.Engine
+.. _sqlalchemy.orm.Session: https://docs.sqlalchemy.org/en/latest/orm/session_api.html#sqlalchemy.orm.Session
--- src/zundamahjong/database/__init__.py
+++ src/zundamahjong/database/__init__.py
@@ -6,14 +6,42 @@ from .models import Base as Base
 
 
 class SQLAlchemy:
-    def __init__(self, engine: sa.Engine):
+    """Helper class to manage SQLAlchemy ORM sessions for the game server.
+
+    :param engine:
+    :type engine: `sqlalchemy.Engine`_
+
+    Example usage::
+
+        from ..database import db
+        from ..database.models import User
+
+        with db.session.begin():
+            db.session.add(User(name="Zundamon", password=password))
+        # commits the transaction
+
+    """
+
+    def __init__(self, engine: sa.Engine) -> None:
         self._Session = scoped_session(sessionmaker(engine, autobegin=False))
 
     @property
     def session(self) -> Session:
+        """Handle to an ORM session open for the current Python thread.
+
+        :rtype: `sqlalchemy.orm.Session`_
+
+        """
+
         return self._Session()
 
     def close(self) -> None:
+        """Close ORM session (if any) for the current Python thread. In
+        practice this means keeping track of one ORM session per connected
+        Socket.IO client.
+
+        """
+
         self._Session.remove()
 
 
