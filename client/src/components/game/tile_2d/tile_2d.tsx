821b3d7c3b585af821b3dcf5b65113ca0d6e82eb
faraplay <faraplay@outlook.com>
2025-10-18 17:16:16 +0100

    use flipped tile in closed kan on win screen

--- client/src/components/game/tile_2d/tile_2d.tsx
+++ client/src/components/game/tile_2d/tile_2d.tsx
@@ -6,10 +6,20 @@ import "./tile_2d.css";
 
 export function Tile2D({ tile }: { tile: TileId }) {
   return (
-    <span class="tile_div tile_2d">
+    <span class="tile_div tile_2d tile_2d_front">
       <div class="tile_back_layer" />
       <div class="tile_middle_layer" />
       <TileImage tile={tile} />
+    </span>
+  );
+}
+
+export function Tile2DBack() {
+  return (
+    <span class="tile_div tile_2d tile_2d_back">
+      <div class="tile_back_layer" />
+      <div class="tile_middle_layer" />
+      <div class="tile_front_layer" />
     </span>
   );
 }
