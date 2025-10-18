63eac5c22d20ada0247837d3a482d13cd7a93ac0
Alex Epelde <alex@epelde.net>
2025-10-18 04:10:13 -0400

    even more involved decoration on `sio_on()`

--- src/zundamahjong/server/sio.py
+++ src/zundamahjong/server/sio.py
@@ -1,6 +1,9 @@
+# pyright: reportIgnoreCommentWithoutRule=false
+
 from collections.abc import Callable
 import logging
-from typing import Any, Optional
+from typing import Optional, TypeVar
+from typing_extensions import Concatenate, ParamSpec
 
 from flask import request
 from flask_socketio import SocketIO as _SocketIO
@@ -24,28 +27,32 @@ class SocketIO(_SocketIO):  # type: ignore[misc]
 
 sio = SocketIO(app, logger=logger, async_mode="threading")
 
-Handler = Callable[..., Optional[Any]]
+P = ParamSpec("P")
+T = TypeVar("T")
+Handler = Callable[Concatenate[str, P], Optional[T]]
 
 
-def sio_on(event: str) -> Callable[[Handler], Handler]:
+def sio_on(event: str) -> Callable[[Handler[P, T]], Callable[P, Optional[T]]]:
     def sio_on_decorator(
-        handler: Handler,
-    ) -> Handler:
-        def wrapped_handler(*args: Any) -> Optional[Any]:
-            sid: str = request.sid  # type: ignore
-            try:
-                logger.debug(
-                    f"Received event {event} from {sid} with args {repr(args)}"
-                )
-                return_value = handler(sid, *args)
-                logger.debug(
-                    f"Handler for event {event} from {sid} returned {return_value}"
-                )
-                return return_value
-            except Exception as e:
-                sio.emit_error(str(e), to=sid)
-                logger.exception(e)
-            return None
+        handler: Handler[P, T],
+    ) -> Callable[P, Optional[T]]:
+        def wrapped_handler(*args: P.args, **kwargs: P.kwargs) -> Optional[T]:
+            with app.app_context():
+                sid: str = request.sid  # type: ignore  # pyright: ignore
+                assert isinstance(sid, str)
+                try:
+                    logger.debug(
+                        f"Received event {event} from {sid} with args {repr(args)}"
+                    )
+                    return_value = handler(sid, *args, **kwargs)
+                    logger.debug(
+                        f"Handler for event {event} from {sid} returned {return_value}"
+                    )
+                    return return_value
+                except Exception as e:
+                    sio.emit_error(str(e), to=sid)
+                    logger.exception(e)
+                return None
 
         sio.on_event(event, wrapped_handler)  # pyright: ignore[reportArgumentType]
         return wrapped_handler
