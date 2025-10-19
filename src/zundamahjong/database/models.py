5fe898a35ad7010196b6457078bfc6efc0306a6a
Alex Epelde <alex@epelde.net>
2025-10-18 23:36:52 -0400

    db model annotations

--- src/zundamahjong/database/models.py
+++ src/zundamahjong/database/models.py
@@ -1,23 +1,41 @@
-from typing import final
+from typing import ClassVar, final
 
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from ..types.avatar import Avatar
 
 
 class Base(DeclarativeBase):
-    pass
+    """TODO"""
+
+    metadata: ClassVar[MetaData]
+    """TODO
+
+    Example usage::
+
+        from ..database import Base, engine
+
+        # Populate database with tables.
+        Base.metadata.create_all(engine)
+
+    """
 
 
 @final
 class User(Base):
+    """TODO"""
+
     __tablename__ = "user_account"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    """Unique id value used as primary key."""
+
     name: Mapped[str] = mapped_column(unique=True)
+    """User's chosen login name."""
 
-    """Hashed user password."""
     password: Mapped[str] = mapped_column()
+    """User's hashed password."""
 
-    """User's chosen avatar."""
     avatar: Mapped[Avatar] = mapped_column(server_default=("zundamon"))
+    """User's chosen avatar."""
