0a10d46d5777763bfad087e012dac9b0e0c35f03
Alex Epelde <alex@epelde.net>
2025-10-19 11:02:32 -0400

    rename wsgi_app to flask_app
    Strictly speaking, the real wsgi_app is flask_app.wsgi_app

--- src/zundamahjong/server/flask.py
+++ src/zundamahjong/server/flask.py
@@ -1,16 +1,16 @@
 from flask import Flask, redirect, url_for
 from werkzeug.wrappers import Response
 
-wsgi_app = Flask(
+flask_app = Flask(
     "zundamahjong", static_url_path="/zundamahjong/", static_folder="client"
 )
 
 
-@wsgi_app.route("/")
+@flask_app.route("/")
 def base() -> Response:
     return redirect(url_for("index"))
 
 
-@wsgi_app.route("/zundamahjong/")
+@flask_app.route("/zundamahjong/")
 def index() -> Response:
-    return wsgi_app.send_static_file("index.html")
+    return flask_app.send_static_file("index.html")
