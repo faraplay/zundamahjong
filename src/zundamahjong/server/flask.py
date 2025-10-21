f8c8b7eeaf2cbee440898d576bbb10894c5d927c
Alex Epelde <alex@epelde.net>
2025-10-21 02:17:33 -0400

    let's try flask-socketio

--- src/zundamahjong/server/flask.py
+++ src/zundamahjong/server/flask.py
@@ -3,18 +3,16 @@ from werkzeug.wrappers import Response
 
 from ..database import db
 
-flask_app = Flask(
-    "zundamahjong", static_url_path="/zundamahjong/", static_folder="client"
-)
+app = Flask("zundamahjong", static_url_path="/zundamahjong/", static_folder="client")
 
-db.init_app(flask_app)
+db.init_app(app)
 
 
-@flask_app.route("/")
+@app.route("/")
 def base() -> Response:
     return redirect(url_for("index"))
 
 
-@flask_app.route("/zundamahjong/")
+@app.route("/zundamahjong/")
 def index() -> Response:
-    return flask_app.send_static_file("index.html")
+    return app.send_static_file("index.html")
