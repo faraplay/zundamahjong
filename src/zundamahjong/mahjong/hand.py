76d726c7d7bb5efa329bb887884e580a6c0d21b5
faraplay <faraplay@outlook.com>
2025-11-06 22:15:37 +0000

    fix riichi behaviour

--- src/zundamahjong/mahjong/hand.py
+++ src/zundamahjong/mahjong/hand.py
@@ -48,7 +48,7 @@ class Hand:
         return self._tiles
 
     @property
-    def open_tiles(self) -> list[TileId]:
+    def call_tiles(self) -> list[TileId]:
         return [tile for call in self._calls for tile in get_call_tiles(call)]
 
     @property
@@ -110,6 +110,9 @@ class Hand:
         ]
 
     def get_chiis(self, last_discard: TileId) -> list[Action]:
+        if self.is_riichi:
+            return []
+
         discard_value = get_tile_value(last_discard)
         actions: list[Action] = []
         if not is_number(discard_value):
@@ -168,6 +171,9 @@ class Hand:
         self._waits = None
 
     def get_pons(self, last_discard: TileId) -> list[Action]:
+        if self.is_riichi:
+            return []
+
         discard_value = get_tile_value(last_discard)
         actions: list[Action] = []
         same_tiles = [
@@ -201,6 +207,9 @@ class Hand:
         self._waits = None
 
     def get_open_kans(self, last_discard: TileId) -> list[Action]:
+        if self.is_riichi:
+            return []
+
         discard_value = get_tile_value(last_discard)
         actions: list[Action] = []
         same_tiles = [
@@ -264,8 +273,21 @@ class Hand:
         self._waits = None
 
     def get_closed_kans(self) -> list[Action]:
-        actions: list[Action] = []
         tile_value_buckets = get_tile_value_buckets(self._tiles)
+        if self.is_riichi:
+            last_tile_value = get_tile_value(self._tiles[-1])
+            bucket = tile_value_buckets[last_tile_value]
+            if len(bucket) >= 4:
+                kan_tiles = (bucket[0], bucket[1], bucket[2], bucket[3])
+                current_waits = self._calculate_waits(self._tiles[:-1])
+                tiles_copy = self._tiles.copy()
+                for tile in kan_tiles:
+                    tiles_copy.remove(tile)
+                new_waits = self._calculate_waits(tiles_copy)
+                if current_waits == new_waits:
+                    return [ClosedKanAction(tiles=kan_tiles)]
+            return []
+        actions: list[Action] = []
         for bucket in tile_value_buckets.values():
             if len(bucket) >= 4:
                 bucket.sort()
@@ -307,14 +329,14 @@ class Hand:
             self._waits = self._calculate_waits(self._tiles)
         return self._waits
 
-    def _calculate_waits(self, closed_tiles: list[TileId]) -> frozenset[TileValue]:
-        if len(closed_tiles) % 3 != 1:
+    def _calculate_waits(self, hand_tiles: list[TileId]) -> frozenset[TileValue]:
+        if len(hand_tiles) % 3 != 1:
             return frozenset()
-        all_tiles_buckets = get_tile_value_buckets(closed_tiles + self.open_tiles)
+        all_tiles_buckets = get_tile_value_buckets(hand_tiles + self.call_tiles)
         unusable_tile_values = {
             tileValue
             for (tileValue, tiles) in all_tiles_buckets.items()
             if len(tiles) >= 4
         }
-        waits = get_waits(get_tile_values(closed_tiles))
+        waits = get_waits(get_tile_values(hand_tiles))
         return waits - unusable_tile_values
