15d478f7deade32bb6ebf339254639bb1aecb069
faraplay <faraplay@outlook.com>
2025-11-14 12:45:48 +0000

    separate winning tile in win hand

--- client/src/components/game/win_hand/win_hand.tsx
+++ client/src/components/game/win_hand/win_hand.tsx
@@ -29,18 +29,18 @@ export function WinHand({ win_info }: { win_info: Win | null }) {
   }
   const win_hand_width = win_info.calls.reduce(
     (partial_sum, call) => partial_sum + getCallTiles(call).length + 0.5,
-    win_info.hand.length,
+    win_info.hand.length + 0.5,
   );
   const flowerOverlap = win_hand_width + win_info.flowers.length >= 21.5;
   return (
-    <div id="win_hand">
-      <div id="win_flowers" class={flowerOverlap ? "overlap" : ""}>
+    <div class="win_hand">
+      <div class={`win_flowers ${flowerOverlap ? "overlap" : ""}`}>
         <Tile2DList tiles={win_info.flowers} />
       </div>
-      <div id="win_tiles">
+      <div class="win_tiles">
         <Tile2DList tiles={win_info.hand} />
       </div>
-      <div id="win_calls">
+      <div class="win_calls">
         {win_info.calls.map((call) => (
           <WinCall key={call} call={call} />
         ))}
