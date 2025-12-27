fd1eaf3926cf0cf4053b1715ce4942dcc32857c2
faraplay <faraplay@outlook.com>
2025-12-27 19:14:56 +0000

    add docstrings for deck, call, meld

--- src/zundamahjong/mahjong/call.py
+++ src/zundamahjong/mahjong/call.py
@@ -8,6 +8,8 @@ from .tile import TileId
 
 
 class CallType(IntEnum):
+    "Enum representing the type of call (chii, pon, open kan, added kan, closed kan)"
+
     CHI = 0
     PON = 1
     OPEN_KAN = 2
@@ -22,46 +24,79 @@ call_meld_types = {
     CallType.ADD_KAN: MeldType.KAN,
     CallType.CLOSED_KAN: MeldType.KAN,
 }
+"Dictionary containing the meld type for each :py:class:`CallType`."
 
 
 def get_meld_type(call_type: CallType) -> MeldType:
+    "Returns the meld type (chii/pon/kan) of a :py:class:`CallType`."
     return call_meld_types[call_type]
 
 
 class OpenCall(BaseModel, frozen=True):
+    "Represents a chii or pon call."
+
     call_type: Literal[CallType.CHI, CallType.PON]
+    "The :py:class:`CallType` of the call."
     called_player_index: int
+    "The index of the player who discarded the called tile."
     called_tile: TileId
+    "The :py:class:`TileId` of the called tile."
     other_tiles: tuple[TileId, TileId]
+    "The other :py:class:`TileId` s used to make the meld."
 
 
 class OpenKanCall(BaseModel, frozen=True):
+    "Represents an open kan call."
+
     call_type: Literal[CallType.OPEN_KAN] = CallType.OPEN_KAN
+    "The :py:class:`CallType` of the call."
     called_player_index: int
+    "The index of the player who discarded the called tile."
     called_tile: TileId
+    "The :py:class:`TileId` of the called tile."
     other_tiles: tuple[TileId, TileId, TileId]
+    "The other :py:class:`TileId` s used to make the meld."
 
 
 class AddKanCall(BaseModel, frozen=True):
+    "Represents an added kan call."
+
     call_type: Literal[CallType.ADD_KAN] = CallType.ADD_KAN
+    "The :py:class:`CallType` of the call."
     called_player_index: int
+    "The index of the player who discarded the called tile."
     called_tile: TileId
+    "The :py:class:`TileId` of the called tile."
     added_tile: TileId
+    "The :py:class:`TileId` of the added tile."
     other_tiles: tuple[TileId, TileId]
+    "The other :py:class:`TileId` s used to make the meld."
 
 
 class ClosedKanCall(BaseModel, frozen=True):
+    "Represents a closed kan call."
+
     call_type: Literal[CallType.CLOSED_KAN] = CallType.CLOSED_KAN
+    "The :py:class:`CallType` of the call."
     tiles: tuple[TileId, TileId, TileId, TileId]
+    "The :py:class:`TileId` s used to make the closed kan."
 
 
 Call = Annotated[
     OpenCall | OpenKanCall | AddKanCall | ClosedKanCall,
     Field(discriminator="call_type"),
 ]
+"""
+Represents a call made by a player.
+Contains the call type and the tiles of the call.
+"""
 
 
 def get_call_tiles(call: Call) -> list[TileId]:
+    """
+    Returns a list of the :py:class:`TileId` s that
+    make up the given :py:class:`Call`.
+    """
     if call.call_type == CallType.CLOSED_KAN:
         return list(call.tiles)
     elif call.call_type == CallType.ADD_KAN:
--- src/zundamahjong/mahjong/deck.py
+++ src/zundamahjong/mahjong/deck.py
@@ -17,9 +17,16 @@ four_player_deck: list[TileId] = [
     )
     for r in range(4)
 ]
+"""
+A list of :py:class:`TileId` s of the tiles used in a four-player deck
+without flowers.
+"""
 four_player_flowers: list[TileId] = [
     tile_value * N for tile_value in (list(range(41, 49)))
 ]
+"""
+A list of :py:class:`TileId` s of the flowers used in a four-player deck.
+"""
 
 three_player_deck: list[TileId] = [
     tile_value * N + r
@@ -28,28 +35,57 @@ three_player_deck: list[TileId] = [
     )
     for r in range(4)
 ]
+"""
+A list of :py:class:`TileId` s of the tiles used in a three-player deck
+without flowers.
+"""
 three_player_flowers: list[TileId] = [
     tile_value * N for tile_value in [41, 42, 43, 45, 46, 47]
 ]
+"""
+A list of :py:class:`TileId` s of the flowers used in a three-player deck.
+"""
 
 
 @final
 class Deck:
+    """
+    A class representing the deck in a round of mahjong.
+
+    :param tiles: List of :py:class:`TileId` s that the :py:class:`Deck`
+                  will contain.
+    """
+
     def __init__(self, tiles: Iterable[TileId]) -> None:
         self._tiles = deque(tiles)
 
     @classmethod
     def shuffled_deck(cls, tiles: list[TileId]) -> Deck:
+        """
+        Creates a new :py:class:`Deck` containing the given list of
+        :py:class:`TileId` s, shuffled.
+
+        :param tiles: A list of the :py:class:`TileId` s the new
+                      :py:class:`Deck` will contain.
+        """
         new_deck = tiles.copy()
         shuffle(new_deck)
         return cls(new_deck)
 
     @property
     def tiles(self) -> tuple[TileId, ...]:
+        "Returns the list of :py:class:`TileId` s remaining in the deck."
         return tuple(self._tiles)
 
     def pop(self) -> TileId:
+        """
+        Removes the first remaining tile of the deck, and returns its
+        :py:class:`TileId`."""
         return self._tiles.popleft()
 
     def popleft(self) -> TileId:
+        """
+        Removes the tile at the back of the deck, and returns its
+        :py:class:`TileId`.
+        """
         return self._tiles.pop()
