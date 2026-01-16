5737a18ae41217ecc994cfc35e21f43caea61952
faraplay <faraplay@outlook.com>
2026-01-16 12:46:30 +0000

    add new favicon

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
     emptyOutDir: true,
     assetsInlineLimit: (filePath) =>
       filePath.endsWith(".svg") &&
-      !filePath.includes("/bamboo/01") &&
       !filePath.includes("/flower/") &&
       !filePath.includes("/season/"),
     manifest: true,
