a952f66d7408aca7f929e4259aa2e77be9bbb0a9
faraplay <faraplay@outlook.com>
2026-09-15 20:52:02 +0100

    add tests for riichi pot

--- tests/game_test.py
+++ tests/game_test.py
@@ -1,7 +1,12 @@
 import pytest
 
-from tests.decks import test_deck2, test_deck4, test_deck6
-from zundamahjong.mahjong.action import ActionType, HandTileAction, SimpleAction
+from tests.decks import test_deck2, test_deck4, test_deck6, test_deck_riichi
+from zundamahjong.mahjong.action import (
+    ActionType,
+    ClosedKanAction,
+    HandTileAction,
+    SimpleAction,
+)
 from zundamahjong.mahjong.exceptions import InvalidOperationException
 from zundamahjong.mahjong.game import Game
 from zundamahjong.mahjong.game_options import GameOptions
@@ -186,3 +191,91 @@ class TestGame:
         game.round.do_action(0, SimpleAction(action_type=ActionType.TSUMO))
         assert game.win is not None
         assert game.win.draw_count == 2
+
+    def test_riichi_cost(self) -> None:
+        game = Game(
+            options=GameOptions(
+                start_score=500.0, riichi_cost=100.0, can_riichi_negative_score=False
+            ),
+            first_deck_tiles=test_deck_riichi,
+        )
+        round = game.round
+        assert game.player_scores == (500.0, 500.0, 500.0, 500.0)
+        round.do_action(0, HandTileAction(action_type=ActionType.RIICHI, tile=160))
+        assert game.player_scores == (400.0, 500.0, 500.0, 500.0)
+
+    def test_riichi_win_takes_pot(self) -> None:
+        game = Game(
+            options=GameOptions(
+                start_score=500.0, riichi_cost=100.0, can_riichi_negative_score=False
+            ),
+            first_deck_tiles=test_deck_riichi,
+        )
+        round = game.round
+        assert game.player_scores == (500.0, 500.0, 500.0, 500.0)
+        round.do_action(0, HandTileAction(action_type=ActionType.RIICHI, tile=160))
+        assert game.player_scores == (400.0, 500.0, 500.0, 500.0)
+        round.do_action(1, SimpleAction(action_type=ActionType.DRAW))
+        round.do_action(1, HandTileAction(action_type=ActionType.DISCARD, tile=350))
+        round.do_action(2, SimpleAction(action_type=ActionType.DRAW))
+        round.do_action(2, HandTileAction(action_type=ActionType.DISCARD, tile=13))
+        round.do_action(3, SimpleAction(action_type=ActionType.DRAW))
+        round.do_action(3, HandTileAction(action_type=ActionType.DISCARD, tile=223))
+        round.do_action(0, SimpleAction(action_type=ActionType.DRAW))
+        round.do_action(0, SimpleAction(action_type=ActionType.TSUMO))
+        assert game.player_scores == (38900.0, -12300.0, -12300.0, -12300.0)
+
+    def test_too_poor_for_riichi(self) -> None:
+        game = Game(
+            options=GameOptions(
+                start_score=50.0, riichi_cost=100.0, can_riichi_negative_score=False
+            ),
+            first_deck_tiles=test_deck_riichi,
+        )
+        assert game.player_scores == (50.0, 50.0, 50.0, 50.0)
+        assert game.round.allowed_actions[0].actions == [
+            HandTileAction(action_type=ActionType.DISCARD, tile=230),
+            HandTileAction(action_type=ActionType.DISCARD, tile=10),
+            HandTileAction(action_type=ActionType.DISCARD, tile=11),
+            HandTileAction(action_type=ActionType.DISCARD, tile=12),
+            HandTileAction(action_type=ActionType.DISCARD, tile=130),
+            HandTileAction(action_type=ActionType.DISCARD, tile=140),
+            HandTileAction(action_type=ActionType.DISCARD, tile=150),
+            HandTileAction(action_type=ActionType.DISCARD, tile=151),
+            HandTileAction(action_type=ActionType.DISCARD, tile=152),
+            HandTileAction(action_type=ActionType.DISCARD, tile=153),
+            HandTileAction(action_type=ActionType.DISCARD, tile=160),
+            HandTileAction(action_type=ActionType.DISCARD, tile=220),
+            HandTileAction(action_type=ActionType.DISCARD, tile=221),
+            HandTileAction(action_type=ActionType.DISCARD, tile=222),
+            ClosedKanAction(tiles=(150, 151, 152, 153)),
+        ]
+
+    def test_can_riichi_into_negative_score(self) -> None:
+        game = Game(
+            options=GameOptions(
+                start_score=50.0, riichi_cost=100.0, can_riichi_negative_score=True
+            ),
+            first_deck_tiles=test_deck_riichi,
+        )
+        assert game.player_scores == (50.0, 50.0, 50.0, 50.0)
+        assert game.round.allowed_actions[0].actions == [
+            HandTileAction(action_type=ActionType.DISCARD, tile=230),
+            HandTileAction(action_type=ActionType.DISCARD, tile=10),
+            HandTileAction(action_type=ActionType.DISCARD, tile=11),
+            HandTileAction(action_type=ActionType.DISCARD, tile=12),
+            HandTileAction(action_type=ActionType.DISCARD, tile=130),
+            HandTileAction(action_type=ActionType.DISCARD, tile=140),
+            HandTileAction(action_type=ActionType.DISCARD, tile=150),
+            HandTileAction(action_type=ActionType.DISCARD, tile=151),
+            HandTileAction(action_type=ActionType.DISCARD, tile=152),
+            HandTileAction(action_type=ActionType.DISCARD, tile=153),
+            HandTileAction(action_type=ActionType.DISCARD, tile=160),
+            HandTileAction(action_type=ActionType.DISCARD, tile=220),
+            HandTileAction(action_type=ActionType.DISCARD, tile=221),
+            HandTileAction(action_type=ActionType.DISCARD, tile=222),
+            HandTileAction(action_type=ActionType.RIICHI, tile=130),
+            HandTileAction(action_type=ActionType.RIICHI, tile=160),
+            HandTileAction(action_type=ActionType.RIICHI, tile=230),
+            ClosedKanAction(tiles=(150, 151, 152, 153)),
+        ]
